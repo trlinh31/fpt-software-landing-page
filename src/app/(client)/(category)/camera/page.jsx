ea0bc8f10ef4ea161bd5fb25b1369d7cf0ff8855
Linh Dev <nguyentranlinh31@gmail.com>
2025-09-04 22:48:33 +0700

    update

--- src/app/(client)/(category)/camera/page.jsx
+++ src/app/(client)/(category)/camera/page.jsx
@@ -1,31 +1,30 @@
 import HeadingComponent from "@/components/common/heading";
 import ContactComponent from "@/components/contact";
 import ServiceListComponent from "@/components/service-list";
+import { BANNER_IMAGES } from "@/data/banner";
 import React from "react";
 
 export default function CameraPage() {
   return (
     <>
       <section className='space-y-10'>
-        <div className='w-full md:h-[600px] h-[287px]'>
-          <img
-            src='https://s3-api.fpt.vn/fptvn-storage/2025-01-16/1737014670_fptcameraantoanvuottroi.webp'
-            className='w-full h-full object-cover'
-            alt='FPT Software'
-          />
-        </div>
-        <div className='container space-y-10'>
+        <section>
+          <div className='w-full'>
+            <picture>
+              <source media='(min-width: 768px)' srcSet={BANNER_IMAGES[0].desktop} />
+              <img src={BANNER_IMAGES[0].mobile} className='w-full h-full object-cover' alt='FPT Camera' loading='lazy' />
+            </picture>
+          </div>
+          <div className='bg-[#1B1B1B] py-10'>
+            <HeadingComponent title='Vì sao nên chọn FPT Camera' />
+            <div className='container'>
+              <iframe src='https://www.youtube.com/embed/kqlB9X20ClU?autoplay=1&rel=0' className='w-full md:h-[800px]' allowFullScreen></iframe>
+            </div>
+          </div>
+        </section>
+        <div className='space-y-10 container'>
           <ServiceListComponent />
           <ContactComponent />
-        </div>
-        <div className='bg-[#1B1B1B] py-10'>
-          <HeadingComponent title='Vì sao nên chọn FPT Camera' />
-          <div className='container'>
-            <iframe
-              src='https://www.youtube.com/embed/kqlB9X20ClU?autoplay=1&rel=0'
-              className='w-full md:h-[800px]'
-              frameBorder='0'></iframe>
-          </div>
         </div>
       </section>
     </>
--- src/components/category-list.jsx
+++ src/components/category-list.jsx
@@ -7,7 +7,7 @@ export default function CategoryListComponent() {
   return (
     <>
       <HeadingComponent title='Dịch vụ nổi bật' description='Dịch vụ nào hot, ở đây có hết – lướt nhẹ xem thử nha!' />
-      <div className='grid grid-cols-1 md:grid-cols-3 gap-16'>
+      <div className='gap-8 lg:gap-16 grid grid-cols-1 md:grid-cols-3'>
         {CATEGORY_ITEMS.map((item, index) => (
           <CategoryCardComponent key={index} category={item} />
         ))}
