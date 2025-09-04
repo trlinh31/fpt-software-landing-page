932487466f59dc8750254538443a031833106f7d
Linh Dev <nguyentranlinh31@gmail.com>
2025-09-04 22:52:24 +0700

    update v2

--- src/components/category-list.jsx
+++ src/components/category-list.jsx
@@ -6,7 +6,7 @@ import { CATEGORY_ITEMS } from "@/data/category";
 export default function CategoryListComponent() {
   return (
     <>
-      <HeadingComponent title='Dịch vụ nổi bật' description='Dịch vụ nào hot, ở đây có hết – lướt nhẹ xem thử nha!' />
+      <HeadingComponent title='Dịch vụ nổi bật' description='Dịch vụ nào hot, ở đây có hết - lướt nhẹ xem thử nha!' />
       <div className='gap-8 lg:gap-16 grid grid-cols-1 md:grid-cols-3'>
         {CATEGORY_ITEMS.map((item, index) => (
           <CategoryCardComponent key={index} category={item} />
