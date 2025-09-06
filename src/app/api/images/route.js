5ee3dbe2f816a3dc9e3663ac8798858babd0067f
Linh Dev <nguyentranlinh31@gmail.com>
2025-09-06 12:53:28 +0700

    fix bug

--- src/app/api/images/route.js
+++ src/app/api/images/route.js
@@ -1,5 +1,7 @@
-import { prisma } from "@/libs/prisma";
 import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function GET(request) {
   const { searchParams } = new URL(request.url);
