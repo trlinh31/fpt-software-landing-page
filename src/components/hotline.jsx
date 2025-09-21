b9e8d89c975a9594aa60ee70cdd1b3a996b5b767
Linh Dev <nguyentranlinh31@gmail.com>
2025-09-21 21:21:12 +0700

    update font size

--- src/components/hotline.jsx
+++ src/components/hotline.jsx
@@ -4,19 +4,19 @@ export default function Hotline() {
   return (
     <div className='text-gray-600'>
       <p className='mb-6 md:mb-10 font-bold text-xl text-center'>
-        Hotline <span className='font-black text-red-600 text-2xl'>{CONTACT_INFORMATION.phone}</span>
+        Hotline <span className='font-black text-red-600 text-xl md:text-2xl'>{CONTACT_INFORMATION.phone}</span>
       </p>
       <p className='mb-0'>Khách hàng có thể đăng ký bằng cách:</p>
       <ul className='space-y-2 my-2 list-disc list-inside'>
         <li>
-          Nhắn tin theo cú pháp: <span className='font-black text-red-600 text-2xl'>DK FPT</span> gửi tới số{" "}
-          <span className='font-black text-red-600 text-2xl'>{CONTACT_INFORMATION.phone}</span>. Trong vòng 5p nhân viên kinh doanh sẽ chủ động liên
-          lạc lại khách hàng để tư vấn gói cước và chương trình khuyến mãi.
+          Nhắn tin theo cú pháp: <span className='font-black text-red-600 text-xl md:text-2xl'>DK FPT</span> gửi tới số{" "}
+          <span className='font-black text-red-600 text-xl md:text-2xl'>{CONTACT_INFORMATION.phone}</span>. Trong vòng 5p nhân viên kinh doanh sẽ chủ
+          động liên lạc lại khách hàng để tư vấn gói cước và chương trình khuyến mãi.
         </li>
         <li>Gọi Hotline bên góc phải màn hình để được tư vấn trực tiếp</li>
         <li>
-          Nhắn tin qua Zalo số <span className='font-black text-red-600 text-2xl'>{CONTACT_INFORMATION.phone}</span> hoặc biểu tượng Zalo bên góc phải
-          màn hình
+          Nhắn tin qua Zalo số <span className='font-black text-red-600 text-xl md:text-2xl'>{CONTACT_INFORMATION.phone}</span> hoặc biểu tượng Zalo
+          bên góc phải màn hình
         </li>
       </ul>
       <p>Thời gian lắp đặt trong vòng 48h sẽ hoàn tất, quy trình như sau:</p>
